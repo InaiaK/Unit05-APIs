c06bd1457ed0d7ab90ae1bee89a01ec99f935d4a
InaiaKenia <inaiakenia15@gmail.com>
2022-03-25 14:40:50 -0600

    JS variables

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+
+var toDo = $('.description');
+var save = $('.saveBtn');
+var textArea = $('.description');
+var timeContainer = $('time-block')
+
 var today = moment();
 $("#currentDay").text(today.format('LLLL'));
 
+function timeContainer(){
+        var isRed =  $('.present');
+        var isGrey = $('.past');
+        var isGreen = $('.future');
 
-
-
-
+}
