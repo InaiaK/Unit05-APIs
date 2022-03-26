4a424a4f7cc52f842241edc657165d8d898c7403
InaiaKenia <inaiakenia15@gmail.com>
2022-03-26 15:04:02 -0600

    Added hours as 24h

--- script.js
+++ script.js
@@ -1,29 +1,55 @@
-var toDo = $('.description');
-var save = $('.saveBtn');
-var textArea = $('.description');
-var timeContainer = $('time-block')
+// Date for the header
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
 
-var today = moment();
-$("#currentDay").text(today.format('LLLL'));
+// Inital function for writing to local storage from input
+$(document).ready(function () {    
+    $(".saveBtn").on("click", function () {        
+        var text = $(this).siblings(".to-do").val();
+        var time = $(this).parent().attr("id");        
+        localStorage.setItem(time, text);
+    })
 
-var hour = $(['#hour09am',
-'#hour10am',
-'#hour11am',
-'#hour12pm',
-'#hour01pm',
-'#hour02pm',
-'#hour03pm',
-'#hour04pm',
-'#hour05pm',
-'#hour06pm',])
+    //track of time based on moment js 
+    function timeTracker() {        
+        var timeNow = moment().hour();        
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
 
-// Present,Past or Future 
-timeContainer.on('click',function() {
-if(today == hour){$('.description')=('.present')}
-else{ $('.description')=('.past','.future')
-}
-if(today < hour){$('.description')=('.past')}
-else{ $('.description')=('.present','.future')
-}
-if(today > hour){$('.description')=('.future')}
-else{ $('.description')=('.present','.past'))}
+            }
+        })
+    }
+// 4: Locaal storage 
+$("#hour9 .to-do").val(localStorage.getItem("hour9"));
+$("#hour10 .to-do").val(localStorage.getItem("hour10"));
+$("#hour11 .to-do").val(localStorage.getItem("hour11"));
+$("#hour12 .to-do").val(localStorage.getItem("hour12"));
+$("#hour13 .to-do").val(localStorage.getItem("hour13"));
+$("#hour14 .to-do").val(localStorage.getItem("hour14"));
+$("#hour15 .to-do").val(localStorage.getItem("hour15"));
+$("#hour16 .to-do").val(localStorage.getItem("hour16"));
+$("#hour17 .to-do").val(localStorage.getItem("hour17"));
+$("#hour18 .to-do").val(localStorage.getItem("hour18"));
+
+timeTracker();
+})
+
+            
+
+
+
+
