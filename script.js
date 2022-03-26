e5f71a6117bd475a907de16070a112d777048845
InaiaKenia <inaiakenia15@gmail.com>
2022-03-26 15:56:25 -0600

    Adding changes and solving conflicts

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Date for the header
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
@@ -55,37 +54,3 @@ $("#hour18 .to-do").val(localStorage.getItem("hour18"));
 
 
 
-=======
-$(document).ready(() => {
-
-    var toDo = $('.description');
-    var save = $('.saveBtn');
-    var textArea = $('.description');
-    var timeContainer = $('time-block')
-
-    var today = moment();
-    $("#currentDay").text(today.format('LLLL'));
-    console.log($('#hour09am')[0].dataset)
-
-    console.log('**********************')
-    const oneHourAgo = moment().subtract('1', 'hour')
-    const oneHourFromNow = moment().add('1', 'hour')
-
-    console.log(moment().isBetween(oneHourAgo, oneHourFromNow))
-    console.log(moment().isBetween(oneHourAgo, oneHourFromNow))
-    console.log(moment().isBetween(oneHourAgo, oneHourFromNow))
-
-    var hourRows = document.getElementsByClassName('hour-row')
-
-    Array.from(hourRows).forEach(hourRow => {
-        if (hourRow.dataset.hour > moment()) {
-            hourRow.style.backgroundColor = 'lightgreen'
-        }
-        // if the hour is between the current hour and the current hour + 1
-
-        // if the hour is more than one hour ahead of the current time
-    })
-    console.log(hourRows)
-
-})
->>>>>>> 7c834c685f9a1604df7651555e206a455491c6ee
